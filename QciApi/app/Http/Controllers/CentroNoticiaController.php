3486c1c190c772aa5e53a7271763f311ab2eac82
mickambar19-Ino <mickambar19@gmail.com>
2016-05-26 00:28:04 -0500

    Termine lo necesario para la Api para entrega 26 de Mayo de 2016

--- QciApi/app/Http/Controllers/CentroNoticiaController.php
+++ QciApi/app/Http/Controllers/CentroNoticiaController.php
@@ -15,24 +15,89 @@ class CentroNoticiaController extends Controller
         return $this->crearRespuestaError("No existe centro con id # $centro_id.",404);
     }
     
-    public function store(Request $request) 
+    public function store(Request $request, $centro_id) 
     {
+        $centro = CentroUniversitario::find($centro_id);
+        if ($centro) 
+        {
+            $this->validacion($request);
+            $campos = $request->all();
+            $campos['vistas']=0;
+            $campos['centro_universitario_id'] = $centro_id;
+            Noticia::create($campos);
+            return $this->crearRespuesta("Se creo correctamente la noticia.",200);
+        }
+        return $this->crearRespuestaError("No se encuentra un centro con id #$centro_id.",404);
     }
     
-    public function update(Request $request, $_id)
+    public function update(Request $request, $centro_id,$noticia_id)
     {
+        $centro = CentroUniversitario::find($centro_id);
+        if ($centro) 
+        {
+            $noticia = Noticia::find($noticia_id);
+            if ($noticia) 
+            {
+                $this->validacion_para_actualizacion($request);
+                $noticia->titulo = $request->get("titulo");
+                $noticia->fecha = $request->get("fecha");
+                $noticia->descripcion = $request->get("descripcion");
+                $noticia->imagen = $request->get("imagen");
+                $noticia->vistas = $request->get("vistas");
+                $noticia->ubicacion = $request->get("ubicacion");
+                $noticia->idautor = $request->get("idautor");
+                $noticia->centro_universitario_id = $centro_id;
+                $noticia->save();
+                return $this->crearRespuesta("Se ha actualizado la noticia id# $noticia_id.",200);
+            }
+            return $this->crearRespuestaError("No existe ningun noticia con id# $noticia_id.",404);
+        }
+        return $this->crearRespuestaError("No existe el centro universitario id#$centro_id",404);
     }
 
-	public function destroy($_id)
+	public function destroy($centro_id, $noticia_id)
     {
+        $noticia = Noticia::find($noticia_id);
+        if ($noticia) 
+        {
+            $noticia->delete();
+            return $this->crearRespuesta("el $noticia->id ha sido eliminado",200);
+            /*if(sizeof($noticia->) > 0)
+            {
+                return $this->crearRespuestaError("La noticia tiene algo asociados",409);
+            }¡*/
+            
+        }
+        return $this->crearRespuestaError("No existe el noticia",404);
     }
 
     public function validacion(Request $request)
     {
         $reglas = 
         [
-            '' => 'required',
-         
+            'titulo' => 'required',
+            'fecha' => 'required',
+            'descripcion' => 'required',
+            'imagen' => 'required',
+            
+            'ubicacion' => 'required',
+            'idautor' => 'required', //recordar que si es 0 el author es anonimo
+        ];
+
+        $this->validate($request, $reglas);
+    }
+    
+    public function validacion_para_actualizacion(Request $request)
+    {
+        $reglas = 
+        [
+            'titulo' => 'required',
+            'fecha' => 'required',
+            'descripcion' => 'required',
+            'imagen' => 'required',
+            'vistas' => 'required|numeric',
+            'ubicacion' => 'required',
+            'idautor' => 'required', //recordar que si es 0 el author es anonimo
         ];
 
         $this->validate($request, $reglas);
---
+++ QciApp/app/src/main/java/qciapp/inoveloper/com/Usuario.java
@@ -0,0 +1,17 @@
+package qciapp.inoveloper.com;
+
+/**
+ * Created by mickambar19 on 5/25/2016.
+ */
+public class Usuario {
+    private String nombreU;
+    private String passwordU;
+
+    public String getPasswordU() {
+        return this.passwordU;
+    }
+
+    public String getNombreU(){
+        return this.nombreU;
+    };
+}
