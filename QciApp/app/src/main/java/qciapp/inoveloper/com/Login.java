2a90b5d3276702612d611eb74fd9da0e2aa31ee8
mickambar19-Ino <mickambar19@gmail.com>
2016-05-26 09:45:43 -0500

    Creado los colores de la app

--- QciApp/app/src/main/java/qciapp/inoveloper/com/Login.java
+++ QciApp/app/src/main/java/qciapp/inoveloper/com/Login.java
@@ -3,9 +3,6 @@ package qciapp.inoveloper.com;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
-
 public class Login extends AppCompatActivity {
 
     @Override
